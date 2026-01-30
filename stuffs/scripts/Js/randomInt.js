41ce180f22a7ccfc821fc57ea35742ff7b27b921
TuongCoffee <172767849+TuongCoffee@users.noreply.github.com>
2026-01-30 20:50:01 +0700

    update stuffs/scripts/Js/randomInt.js

--- stuffs/scripts/Js/randomInt.js
+++ stuffs/scripts/Js/randomInt.js
@@ -1,3 +1,10 @@
 export default function randomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+
+    if (min > max) {
+        throw new Error("min must be <= max");
+    }
+
     return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+}
